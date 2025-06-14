fb7c6f2a1d94a51b00a5d1e197c80fa16e4b4d42
Ganesh Hammad <ganeshhammad837@gmail.com>
2025-06-15 00:18:35 +0530

    crong add

--- src/server.js
+++ src/server.js
@@ -2,13 +2,18 @@ import express from "express";
 import "dotenv/config";
 import { initDB } from "./config/initDB.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
-
+import job from "./config/cron.js";
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json());
+if(process.env.NODE_ENV === "production") job.start()
+  app.get("/api/health", (req, res) => {
+  res.status(200).json({status: "Ok"})
+})
+
 app.set('trust proxy', true);
 app.use("/api/transactions", transactionsRoute);
 app.get('/', (req, res)=>{
